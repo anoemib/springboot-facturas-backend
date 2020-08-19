16bf5f6d37c3fb1df066b0ad139f2b136b9ed7bb
anoemib <alejandro.noemi@gmail.com>
2020-08-19 14:06:31 -0400

    Comit inicial

---
+++ src/test/java/com/factura/springboot/backend/apirest/SpringBootFacturaBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factura.springboot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFacturaBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
